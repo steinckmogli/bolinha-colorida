9646ee83d72ef813d707070309abbfefd985510d
tesla <148909088+steinckmogli@users.noreply.github.com>
2023-10-31 16:59:29 -0300

    adicionando arquivos html, js e css

---
+++ skecth.js
@@ -0,0 +1,35 @@
+let cor;
+let posicaoHorizontal; //x
+let posicaoVertical; //y
+
+function setup() {
+  createCanvas(400, 400);
+  background("gray");
+  cor = color(random(0, 255),random(0, 255),random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  fill(cor);
+
+  if(mouseX < posicaoVertical){
+    posicaoHorizontal --;
+  }
+  
+  if(mouseX > posicaoHorizontal){
+    posicaoHorizontal ++;
+  }
+   if(mouseY < posicaoVertical){
+     posicaoVertical--;
+   }
+   if(mouseY > posicaoVertical){
+     posicaoVertical ++; 
+   }
+
+
+  if(mouseIsPressed){
+    cor = color (random(0,255), random(0, 255), random(0, 255), random (0, 100));
+  }
+}
